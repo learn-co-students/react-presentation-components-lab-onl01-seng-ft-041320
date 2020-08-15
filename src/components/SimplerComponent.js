f52adcf84f4f310f266a3f0da71f34fabc90fa6a
osha7 <ogroetz@gmail.com>
2020-08-15 17:47:27 -0600

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,9 @@
-// Code SimplerComponent Here
+import React from 'react'
+
+const defaultLimit = 100
+	 
+    const SimplerComponent = (props) => <div onClick={props.handleClick}>I am just happy</div>
+
+export default SimplerComponent;
+
+
