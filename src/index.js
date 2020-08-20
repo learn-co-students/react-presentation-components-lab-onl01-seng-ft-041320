465f9ccdccc60eeed49a7d87ed5e3684adacb496
ReginaF2012 <skritschy@gmail.com>
2020-08-20 10:27:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+function handleClick(event){
+  let div = event.target
+  div.innerText === "I am just happy." ? div.innerText = "I am just sad." : div.innerText = "I am just happy."
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
   </div>,
   document.getElementById('root')
 );
